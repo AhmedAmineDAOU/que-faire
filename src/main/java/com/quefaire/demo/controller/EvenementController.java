e1d5b08d66912b292220bc67e19802823b2f50ca
AhmedAmineDAOU <ahmedaminedaou@gmail.com>
2024-10-13 15:43:42 +0200

    ajout de controller/EvenementController.java

---
+++ src/main/java/com/quefaire/demo/controller/EvenementController.java
@@ -0,0 +1,28 @@
+package com.quefaire.demo.controller;
+
+import com.quefaire.demo.entity.Evenement;
+import com.quefaire.demo.service.EvenementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/evenements")
+public class EvenementController {
+
+    @Autowired
+    private EvenementService evenementService;
+
+    @GetMapping
+    public List<Evenement> getAllEvenements() {
+        return evenementService.getAllEvenements();
+    }
+
+    @GetMapping("/{id}")
+    public Evenement getEvenementById(@PathVariable String id) {
+        return evenementService.getEvenementById(id);
+    }
+}
