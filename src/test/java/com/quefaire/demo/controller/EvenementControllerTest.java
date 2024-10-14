333359050e68c0f4afe2d8a7280a92c2e65a64c1
AhmedAmineDAOU <ahmedaminedaou@gmail.com>
2024-10-14 10:35:06 +0200

    generate mockito test classes

---
+++ src/test/java/com/quefaire/demo/controller/EvenementControllerTest.java
@@ -0,0 +1,16 @@
+package com.quefaire.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EvenementControllerTest {
+
+    @Test
+    void getAllEvenements() {
+    }
+
+    @Test
+    void getEvenementById() {
+    }
+}
