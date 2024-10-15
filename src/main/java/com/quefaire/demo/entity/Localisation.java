178612e720e0a6d665da5fcbdabb9d183c5788e5
AhmedAmineDAOU <ahmedaminedaou@gmail.com>
2024-10-15 19:52:41 +0200

    ajout getters and setters pour Localisation

--- src/main/java/com/quefaire/demo/entity/Localisation.java
+++ src/main/java/com/quefaire/demo/entity/Localisation.java
@@ -24,5 +24,67 @@ public class Localisation {
     @JoinColumn(name = "event_id", referencedColumnName = "id")
     private Evenement evenement;
 
-    // Getters et Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddressName() {
+        return addressName;
+    }
+
+    public void setAddressName(String addressName) {
+        this.addressName = addressName;
+    }
+
+    public String getAddressStreet() {
+        return addressStreet;
+    }
+
+    public void setAddressStreet(String addressStreet) {
+        this.addressStreet = addressStreet;
+    }
+
+    public String getAddressZipcode() {
+        return addressZipcode;
+    }
+
+    public void setAddressZipcode(String addressZipcode) {
+        this.addressZipcode = addressZipcode;
+    }
+
+    public String getAddressCity() {
+        return addressCity;
+    }
+
+    public void setAddressCity(String addressCity) {
+        this.addressCity = addressCity;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Evenement getEvenement() {
+        return evenement;
+    }
+
+    public void setEvenement(Evenement evenement) {
+        this.evenement = evenement;
+    }
 }
