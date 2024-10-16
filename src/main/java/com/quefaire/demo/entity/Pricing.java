c7f52d15a59763d29347a1bc20a4b8bdf04dc2af
AhmedAmineDAOU <ahmedaminedaou@gmail.com>
2024-10-16 12:06:47 +0200

    add pricing entity

---
+++ src/main/java/com/quefaire/demo/entity/Pricing.java
@@ -0,0 +1,51 @@
+package com.quefaire.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pricing")
+public class Pricing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String priceType;
+
+    @Lob
+    private String priceDetail;
+    
+    private Event event;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPriceType() {
+        return priceType;
+    }
+
+    public void setPriceType(String priceType) {
+        this.priceType = priceType;
+    }
+
+    public String getPriceDetail() {
+        return priceDetail;
+    }
+
+    public void setPriceDetail(String priceDetail) {
+        this.priceDetail = priceDetail;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event Event) {
+        this.event = Event;
+    }
+}
